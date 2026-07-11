d2474ad75104742b87349e68486f434e077184d7
Jakob Fix <170240+jfix@users.noreply.github.com>
2026-07-11 22:59:49 +0200

    Test exact HAL SWORD media type headers

--- tests/test_sword_headers.py
+++ tests/test_sword_headers.py
@@ -1,3 +1,4 @@
+from hal_assistant.hal_xml import PACKAGING
 from hal_assistant.sword import _headers
 
 
@@ -12,10 +13,14 @@ def test_sword_headers_preserve_existing_affiliations() -> None:
     assert headers["LoadFilter"] == "noaffiliation"
     assert headers["X-test"] == "1"
     assert headers["On-Behalf-Of"] == "idhal|florence-fix"
+    assert headers["Content-Type"] == "text/xml"
+    assert headers["Packaging"] == PACKAGING
 
 
 def test_production_headers_also_preserve_existing_affiliations() -> None:
     headers = _headers("login", "password", test=False, on_behalf_of=None)
 
     assert headers["LoadFilter"] == "noaffiliation"
+    assert headers["Content-Type"] == "text/xml"
+    assert headers["Packaging"] == PACKAGING
     assert "X-test" not in headers
